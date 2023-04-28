9656afc1bba40bd777598292efb90796499a4363
Dr Vipin Kumar <geniusvipin@gmail.com>
2023-04-28 16:17:49 +0530

    Update on 28 April, 2023

--- CoreJava/src/inheritance/InheritanceEx2.java
+++ CoreJava/src/inheritance/InheritanceEx2.java
@@ -37,7 +37,7 @@ public class InheritanceEx2 {
 		// TODO Auto-generated method stub
            A a1 =new A("Message for A");
            a1.display();
-           a1.display("Message by A Block");
+          // a1.display("Message by A Block");
            B b1 =new B("Message for B");
            b1.display();
            b1.display("Message by B Block");
