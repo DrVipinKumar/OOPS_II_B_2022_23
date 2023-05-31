8891d40355d0b81afb334f52af34c23e98ff232d
Dr Vipin Kumar <geniusvipin@gmail.com>
2023-05-31 11:45:57 +0530

    updated on 31 May, 2023

---
+++ CoreJava/src/multithreading/Example1.java
@@ -0,0 +1,40 @@
+package multithreading;
+class ThreadB implements Runnable {
+	
+	public void run() {
+		for(int i=0;i<5;i++) {
+			System.out.println(Thread.currentThread().getName()+
+					" Priority="+Thread.currentThread().getPriority());
+			
+		}
+	}
+}
+
+class ThreadA extends Thread {
+	ThreadA(String name){
+		super(name);
+	}
+	public void run() {
+		for(int i=0;i<5;i++) {
+			System.out.println(Thread.currentThread().getName()+
+					" Priority="+Thread.currentThread().getPriority());
+			
+		}
+	}
+}
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Thread.currentThread().getName());
+		ThreadA t1 =new ThreadA("Thread A");
+		t1.setPriority(10);
+		t1.start();
+		ThreadB r1 =new ThreadB();
+		Thread t2 =new Thread(r1,"Thread B");
+		t2.setDaemon(true);
+		t2.start();
+		
+	}
+
+}
